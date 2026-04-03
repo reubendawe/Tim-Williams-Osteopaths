1a0e90862d17d1f2615b1da972e23de55abcd451
Reuben Dawe <reubendawemarketing@gmail.com>
2026-04-03 18:10:05 +0100

    Experience section updates

--- template-parts/blocks/experience.php
+++ template-parts/blocks/experience.php
@@ -12,7 +12,7 @@
                 <h2><u>Years</u> of proven practice...</h2>
             </div>
         </div>
-        <div class="row g-3">
+        <div class="row g-3 experience-imgs">
             <div class="col-6 col-md-4">
                 <img src="https://placehold.jp/409x630.png" class="img-fluid">
             </div>
@@ -22,6 +22,11 @@
             <div class="col-12 col-md-4">
                 <img src="https://placehold.jp/409x630.png" class="img-fluid">
             </div>
+        </div>
+        <div class="row">
+            <p>I trained at the British School of osteopathy, qualifying in 1977. I have worked in Lyme Regis and Bridport since. I spent ten years up to 2004 teaching at the Osteopathic Centre for Children.</p>
+            <p>For the last twenty years I have been interested in treating patients with Chronic fatigue and Lyme disease. This has led me from studying the Perrin Technique with Ray Perrin https://theperrintechnique.com/, training with Dr Dietrich Klinghardt https://klinghardtinstitute.com/ in the US and Dr Tony Smith, who developed a treatment called Lymestop, https://craniobiotic.com/</p>
+            <p>In the last ten years I have seen patients in London as patients come to see me from further afield. In 2024 I was discovered on Facebook by patients overseas and travelled to see patients in Dublin, Bonn in Germany and Spain.</p>
         </div>
     </div>
 
