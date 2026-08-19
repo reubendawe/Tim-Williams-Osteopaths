cd6e8833604cfe558e4332dc33d85b8a1553cfc2
Reuben <reubendawemarketing@gmail.com>
2026-08-19 18:15:02 +0100

    Basic footer created

--- footer.php
+++ footer.php
@@ -1,5 +1,66 @@
 <footer class="site-footer">
-</footer>
+    <div class="site-footer__container">
 
-    <?php wp_footer(); ?> </body>
-</html>
+        <div class="site-footer__top">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-footer__brand">
+                Tim Williams Osteopaths
+            </a>
+
+            <nav class="site-footer__nav" aria-label="Footer navigation">
+                <ul class="site-footer__nav-list">
+                    <li class="site-footer__nav-item">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                            Home
+                        </a>
+                    </li>
+
+                    <li class="site-footer__nav-item">
+                        <a href="<?php echo esc_url( home_url( '/about/' ) ); ?>">
+                            About
+                        </a>
+                    </li>
+
+                    <li class="site-footer__nav-item">
+                        <a href="<?php echo esc_url( home_url( '/testimonials/' ) ); ?>">
+                            Testimonials
+                        </a>
+                    </li>
+
+                    <li class="site-footer__nav-item">
+                        <a href="<?php echo esc_url( home_url( '/services/' ) ); ?>">
+                            Services
+                        </a>
+                    </li>
+
+                    <li class="site-footer__nav-item">
+                        <a href="<?php echo esc_url( home_url( '/contact/' ) ); ?>">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="site-footer__divider"></div>
+
+        <div class="site-footer__bottom">
+
+            <p class="site-footer__copyright">
+                &copy; <?php echo esc_html( date( 'Y' ) ); ?>
+                Reuben Dawe UI &amp; Web Development. All rights reserved.
+            </p>
+
+            <nav class="site-footer__legal" aria-label="Legal navigation">
+                <a href="<?php echo esc_url( home_url( '/privacy-policy/' ) ); ?>">
+                    Privacy Policy
+                </a>
+
+                <a href="<?php echo esc_url( home_url( '/terms-of-service/' ) ); ?>">
+                    Terms of Service
+                </a>
+            </nav>
+
+        </div>
+
+    </div>
+</footer>
